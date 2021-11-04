ccf42d1b32783f53cb416779743aa3eecc902fd3
lilprim <79355252+lilprim@users.noreply.github.com>
2021-11-04 10:03:56 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/cf4023b3a4.js" crossorigin="anonymous"></script>
+
+    <title>Human Responsibility</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<body>
+    
+    <header>
+        <!-- top page-->
+        <div class="container-fluid top_header">
+                <img class=logo src="/Logo Human Modif by Kev/Logo_main_terre_feuille.png" alt="">
+
+                <!-- Navigation-->
+                    <div class="row navigation">
+                        <div class="col-12">
+                    <a class="bouton" href="qui-sommes-nous.html">Qui sommes nous ?</a>
+                    <a class="bouton " href="notre-charte.html">Notre charte</a>
+                    <a class="bouton" href="nous-contacter.html">Nous contacter</a>
+                    <a class="bouton" href="login.php">Se connecter</a>
+                    </div>
+                        </div>
+        </div>
+        <div class="container-fluid barre_top">
+
+        </div>
+        <!-- Image HRW -->
+                <div class="row titre_logo">
+                    <img class="titre_logo" src="/Logo Human Modif by Kev/logo_Typo.png" alt="">
+                </div>
+
+        
+            
+    </header>
+    
